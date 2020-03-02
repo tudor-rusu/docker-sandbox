e80c3c3e1795b5a3fc772e0ed98b82bc67caa8fb
Tudor Rusu <tudor.rusu@sitewards.com>
2020-03-02 11:46:39 +0100

    19 feat docker_mastery: starting a nginx web server

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@
 
 set -eu
 
-docker version
-docker info
-docker --help
+#docker container run --publish 80:80 nginx
+docker container run --publish 80:80 --detach nginx
+docker container ls
+docker container stop 5d0a0
+docker container ls
+docker container ls -a
+docker container run -p 80:80 -d --name webhost nginx
+docker container ls -a
+docker container logs webhost
+docker container top webhost
+docker container ls -a
+docker container rm -f 0312 5d0a 2ca0
