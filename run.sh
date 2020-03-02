72c85b736cd5866fbb3ac810b03401d55d15f095
Tudor Rusu <tudor.rusu@sitewards.com>
2020-03-02 15:29:32 +0100

    26 feta docker_mastery: getting a shell inside

--- run.sh
+++ run.sh
@@ -5,13 +5,16 @@
 set -eu
 
 docker container ls -a
-docker container run -p 80:80 -d --name nginx nginx
-docker container run -d -p 3306:3306 --name db -e MYSQL_RANDOM_ROOT_PASSWORD=yes mysql
-docker container logs db
-# 2020-03-02 13:37:45+00:00 [Note] [Entrypoint]: GENERATED ROOT PASSWORD: sha6quuFooGhouNaequahpheo2JoPhei
-docker container top db
-docker container inspect nginx
-docker container stats
+docker container run -it --name proxy nginx bash
+docker container run -it --name ubuntu ubuntu bash
+#root@0e4f37dfedaa:/# apt-get update
+#root@0e4f37dfedaa:/# apt-get install -y curl
+#root@0e4f37dfedaa:/# curl google.com
+#root@0e4f37dfedaa:/# exit
+docker container start -ai ubuntu
+#root@0e4f37dfedaa:/# curl siteards.com
+#root@0e4f37dfedaa:/# exit
+docker container start ubuntu
+docker container exec -it ubuntu ps aux
 docker container ls -a
-docker container rm -f 098d 78c5 519e
-docker container ls -a
+docker container rm -f 0e4f3 eb6533
