26ac3b6a4baf09459f279a52905bba158a36bb02
Tudor Rusu <tudor.rusu@sitewards.com>
2020-03-03 11:39:39 +0100

    30 feat docker_mastery: docker networks DNS

--- run.sh
+++ run.sh
@@ -6,39 +6,18 @@ set -eu
 
 docker container ls -a
 docker network ls
-docker network inspect bridge
-#"Containers": {
-#            "015e134940414389fe18252e5b42a73596f84db555f252e0f1ca5970ff379a5a": {
-#                "Name": "webhost",
-#                "EndpointID": "94be114a01a4c5022299a8bfabff4619bab391e26185f4116d0a01b3d4bfcb9c",
-#                "MacAddress": "02:42:ac:11:00:02",
-#                "IPv4Address": "172.17.0.2/16",
-#                "IPv6Address": ""
-#            }
-#        },
-#"IPAM": {
-#            "Driver": "default",
-#            "Options": null,
-#            "Config": [
-#                {
-#                    "Subnet": "172.17.0.0/16",
-#                    "Gateway": "172.17.0.1"
-#                }
-#            ]
-#        },
-docker network create my_mage2_app
-#5ccde48590b64a7ab07eb7188f11dfb194eea9a7893c4b01a2f27d569dadb3aa
-docker network ls
-docker container run -d --name new_nginx --network my_mage2_app nginx:alpine
-#Unable to find image 'nginx:alpine' locally
-#alpine: Pulling from library/nginx
-#4167d3e14976: Pull complete
-#db94a93dfca0: Pull complete
-#Digest: sha256:9e81b8f9cef5a095f892183688798a5b2c368663276aa0f2be4b1cd283ace53d
-#Status: Downloaded newer image for nginx:alpine
-#e09f032f2ebcf97469998b4459a24278e8c540d42ac7fc29d1f47e017bf4e8f6
 docker network inspect my_mage2_app
-#"Containers": {
+docker container run -d --name my_nginx --network my_mage2_app nginx:alpine
+#8bfd7981d9f438dd0ec001756c4c3ed72d9e39c4b851858a0ab3ceec46838272
+docker network inspect my_mage2_app
+# "Containers": {
+#            "8bfd7981d9f438dd0ec001756c4c3ed72d9e39c4b851858a0ab3ceec46838272": {
+#                "Name": "my_nginx",
+#                "EndpointID": "79e6d4410dc4227b6c5bc4be12ddcc4fde7f539e44efd8606087817895b964cf",
+#                "MacAddress": "02:42:ac:12:00:03",
+#                "IPv4Address": "172.18.0.3/16",
+#                "IPv6Address": ""
+#            },
 #            "e09f032f2ebcf97469998b4459a24278e8c540d42ac7fc29d1f47e017bf4e8f6": {
 #                "Name": "new_nginx",
 #                "EndpointID": "7dea7281cdc81a71b13c65b4510a5b3e4306859f23c6e2e6ddebe24a6dee7342",
@@ -47,39 +26,9 @@ docker network inspect my_mage2_app
 #                "IPv6Address": ""
 #            }
 #        },
-docker network connect my_mage2_app webhost
-docker container inspect webhost
-#"Networks": {
-#                "bridge": {
-#                    "IPAMConfig": null,
-#                    "Links": null,
-#                    "Aliases": null,
-#                    "NetworkID": "f8bfecc3ae41ef36a26da424ad4a0fa90b254df32552651792103f035e4e2dde",
-#                    "EndpointID": "94be114a01a4c5022299a8bfabff4619bab391e26185f4116d0a01b3d4bfcb9c",
-#                    "Gateway": "172.17.0.1",
-#                    "IPAddress": "172.17.0.2",
-#                    "IPPrefixLen": 16,
-#                    "IPv6Gateway": "",
-#                    "GlobalIPv6Address": "",
-#                    "GlobalIPv6PrefixLen": 0,
-#                    "MacAddress": "02:42:ac:11:00:02",
-#                    "DriverOpts": null
-#                },
-#                "my_mage2_app": {
-#                    "IPAMConfig": {},
-#                    "Links": null,
-#                    "Aliases": [
-#                        "015e13494041"
-#                    ],
-#                    "NetworkID": "5ccde48590b64a7ab07eb7188f11dfb194eea9a7893c4b01a2f27d569dadb3aa",
-#                    "EndpointID": "7acfa87220cb87a8d7688c150944780cd6a04f07c3a590b4fb11e1409a678000",
-#                    "Gateway": "172.18.0.1",
-#                    "IPAddress": "172.18.0.3",
-#                    "IPPrefixLen": 16,
-#                    "IPv6Gateway": "",
-#                    "GlobalIPv6Address": "",
-#                    "GlobalIPv6PrefixLen": 0,
-#                    "MacAddress": "02:42:ac:12:00:03",
-#                    "DriverOpts": {}
-docker network disconnect my_mage2_app webhost
-docker container inspect webhost
+docker container exec -it my_nginx ping new_nginx
+#PING new_nginx (172.18.0.2): 56 data bytes
+#64 bytes from 172.18.0.2: seq=0 ttl=64 time=0.079 ms
+#64 bytes from 172.18.0.2: seq=1 ttl=64 time=0.135 ms
+#64 bytes from 172.18.0.2: seq=2 ttl=64 time=0.128 ms
+docker container exec -it new_nginx ping my_nginx
