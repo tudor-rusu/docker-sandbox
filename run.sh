bac718bbc94f834cfb4e6d60db314bef9af315a5
Tudor Rusu <tudor.rusu@sitewards.com>
2020-03-05 16:12:22 +0100

    41 feat docker_mastery: extending Official Images

--- run.sh
+++ run.sh
@@ -6,22 +6,22 @@ set -eu
 
 docker container ls -a
 docker image ls
-docker image build -t customnginx ./build/image_testing/
-#....
-#Step 6/7 : EXPOSE 80 443
-# ---> Running in ef1e1b57e1d0
-#Removing intermediate container ef1e1b57e1d0
-# ---> 435c797aef71
-#Step 7/7 : CMD ["nginx", "-g", "daemon off;"]
-# ---> Running in 9519038f4dd2
-#Removing intermediate container 9519038f4dd2
-# ---> cc87b51667f9
-#Successfully built cc87b51667f9
-#Successfully tagged customnginx:latest
+docker container run -p 80:80 --rm nginx
+docker image build -t nginx-with-html ./build/image_extend/
+#Sending build context to Docker daemon  3.072kB
+#Step 1/3 : FROM nginx:latest
+# ---> 6678c7c2e56c
+#Step 2/3 : WORKDIR /usr/share/nginx/html
+# ---> Running in da3507f30bf2
+#Removing intermediate container da3507f30bf2
+# ---> 9d55c95c0172
+#Step 3/3 : COPY index.html index.html
+# ---> 0d714875687a
+#Successfully built 0d714875687a
+#Successfully tagged nginx-with-html:latest
 docker image ls
 #REPOSITORY                                                                TAG                 IMAGE ID            CREATED             SIZE
-#customnginx                                                               latest              cc87b51667f9        2 minutes ago       108MB
-#nginx                                                                     latest              6678c7c2e56c        21 hours ago        127MB
-#tudorrusu/nginx                                                           latest              6678c7c2e56c        21 hours ago        127MB
-#tudorrusu/nginx                                                           testing             6678c7c2e56c        21 hours ago        127MB
-
+#nginx-with-html                                                           latest              0d714875687a        26 seconds ago      127MB
+#customnginx                                                               latest              cc87b51667f9        46 minutes ago      108MB
+docker container run -p 80:80 --rm nginx-with-html
+#on browser http://localhost -----> You just successfully ran a container with a custom file copied into the image at build time!
