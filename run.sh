c36be051d4149b2672e2e0b10dbe4c9049ad0f23
Tudor Rusu <tudor.rusu@sitewards.com>
2020-03-08 13:04:40 +0100

    49 feat docker_mastery: persistend data - bind mounting

--- run.sh
+++ run.sh
@@ -6,11 +6,6 @@ set -eu
 
 docker container ls -a
 docker image ls
-docker container run -d --name mysql -e MYSQL_ROOT_PASSWORD=root mysql
 docker volume ls
-docker container stop mysql
-docker container rm mysql
-docker volume ls
-docker container run -d --name mysql -e MYSQL_ROOT_PASSWORD=root -v mysql-db:/var/lib/mysql mysql
-docker container inspect mysql
-docker volume create
+docker container run -d --name nginx -p 80:80 -v $(pwd):/usr/share/nginx/html nginx
+docker container exec -it nginx bash
